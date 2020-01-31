b52ade356881e5ba3912451d09dc3e23a47c7161
Nicolas Guerrero <saynaysurf@gmail.com>
2020-01-31 15:54:09 -0800

    Finished Part One

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 """Adoption Agency application."""
 from flask import Flask, render_template, request, redirect
 from models import db, connect_db, Pet
-from forms import AddPetForm
+from forms import AddPetForm, EditPetForm
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
@@ -14,26 +14,49 @@ toolbar = DebugToolbarExtension(app)
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 connect_db(app)
-db.create_all()
 
 
 @app.route('/')
 def homepage():
+    """ Show current pets."""
     pets = Pet.query.all()
-    # photo = pets.photo_url or None
 
     return render_template('homepage.html', pets=pets)
 
 
 @app.route('/add', methods=['GET', 'POST'])
 def add_pet_form():
+    """ Show add pet form."""
     form = AddPetForm()
 
     if form.validate_on_submit():
-        name = form.name.data
-        species = form.species.data
-        age = form.age.data
-        notes = form.notes.data
-        available = form.available.data
+        new_pet = Pet(name=form.name.data,
+                      species=form.species.data,
+                      photo_url=form.photo_url.data,
+                      age=form.age.data,
+                      notes=form.notes.data,
+                      available=form.available.data)
+        db.session.add(new_pet)
+        db.session.commit()
+        return redirect('/')
+        
     else:
         return render_template('add_pet_form.html', form=form)
+
+
+@app.route('/<int:pet_id>', methods=['GET', 'POST'])
+def pet_detail(pet_id):
+    """ Show details of pet and form to edit pet."""
+
+    pet = Pet.query.get_or_404(pet_id)
+    form = EditPetForm(obj=pet)
+    
+    if form.validate_on_submit():
+        pet.photo_url = form.photo_url.data
+        pet.notes = form.notes.data
+        pet.available = form.available.data
+        db.session.commit()
+        return redirect('/')
+
+    else:
+        return render_template('pet_detail.html', pet=pet, form=form)
--- forms.py
+++ forms.py
@@ -1,10 +1,25 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, TextAreaField, BooleanField
+from wtforms import StringField, IntegerField, TextAreaField, BooleanField, SelectField
+from wtforms.validators import InputRequired, NumberRange, Optional, URL
 
 
 class AddPetForm(FlaskForm):
-    name = StringField('Pet name')
-    species = StringField('Pet species')
-    age = IntegerField('Age of pet')
-    notes = TextAreaField('Any notes about this pet.')
+    name = StringField('Pet name',
+                       validators=[InputRequired()])
+    species = SelectField('Pet species',
+                          choices=[('cat', 'Cat'), ('dog', 'Dog'), ('porcupine', 'Porcupine')])
+    photo_url = TextAreaField('Image URL',
+                          validators=[Optional(), URL()])
+    age = IntegerField('Age of pet',
+                       validators=[NumberRange(min=0, max=30)])  
+    notes = TextAreaField('Any notes about this pet.',
+                          validators=[Optional()])
     available = BooleanField('Available?')
+
+
+class EditPetForm(FlaskForm):
+    photo_url = TextAreaField('Image URL',
+                          validators=[Optional(), URL()])
+    notes = TextAreaField('Any notes about this pet.',
+                          validators=[Optional()])    
+    available = BooleanField('Available?')                     
